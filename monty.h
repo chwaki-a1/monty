fc1fdd74da2bdcca845f1fa4fb902e785cdb5506
root <chwakik@gmail.com>
2023-01-22 09:04:17 -0800

    first

--- monty.h
+++ monty.h
@@ -53,6 +53,7 @@ typedef struct instruction_s
 } instruction_t;
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
+char getline(void);
 char  *clean_line(char *content);
 void f_push(stack_t **head, unsigned int number);
 void f_pall(stack_t **head, unsigned int number);
